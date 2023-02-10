06468afc07a9e3e0e280873ee139b741f0509ad8
Mtvts <darkhts@hotmail.it>
2023-02-10 21:41:14 +0100

    update testcach

--- altro/testHPC/testcache.c
+++ altro/testHPC/testcache.c
@@ -1,30 +1,72 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+void i386_cpuid_caches () {
+    int i;
+    for (i = 0; i < 32; i++) {
 
-#define KB (1024)
-#define MB (1024 * KB)
-#define GB (1024 * MB)
+        // Variables to hold the contents of the 4 i386 legacy registers
+        uint32_t eax, ebx, ecx, edx; 
 
-#define CACHE_SIZE (512 * MB) // L3 cache size of AMD EPYC 7H12
-#define ARRAY_SIZE (2 * GB)
+        eax = 4; // get cache info
+        ecx = i; // cache id
 
-int main() {
-  int *array = (int *)malloc(ARRAY_SIZE);
-  if (array == NULL) {
-    perror("malloc failed");
-    return -1;
-  }
+        __asm__ (
+            "cpuid" // call i386 cpuid instruction
+            : "+a" (eax) // contains the cpuid command code, 4 for cache query
+            , "=b" (ebx)
+            , "+c" (ecx) // contains the cache id
+            , "=d" (edx)
+        ); // generates output in 4 registers eax, ebx, ecx and edx 
 
-  clock_t start = clock();
+        // See the page 3-191 of the manual.
+        int cache_type = eax & 0x1F; 
 
-  for (int i = 0; i < ARRAY_SIZE / sizeof(int); i += CACHE_SIZE / sizeof(int)) {
-    array[i]++;
-  }
+        if (cache_type == 0) // end of valid cache identifiers
+            break;
 
-  clock_t end = clock();
-  double time_elapsed = ((double)(end - start)) / CLOCKS_PER_SEC;
+        char * cache_type_string;
+        switch (cache_type) {
+            case 1: cache_type_string = "Data Cache"; break;
+            case 2: cache_type_string = "Instruction Cache"; break;
+            case 3: cache_type_string = "Unified Cache"; break;
+            default: cache_type_string = "Unknown Type Cache"; break;
+        }
 
-  printf("Time elapsed: %f seconds\n", time_elapsed);
-  return 0;
+        int cache_level = (eax >>= 5) & 0x7;
+
+        int cache_is_self_initializing = (eax >>= 3) & 0x1; // does not need SW initialization
+        int cache_is_fully_associative = (eax >>= 1) & 0x1;
+
+        // See the page 3-192 of the manual.
+        // ebx contains 3 integers of 10, 10 and 12 bits respectively
+        unsigned int cache_sets = ecx + 1;
+        unsigned int cache_coherency_line_size = (ebx & 0xFFF) + 1;
+        unsigned int cache_physical_line_partitions = ((ebx >>= 12) & 0x3FF) + 1;
+        unsigned int cache_ways_of_associativity = ((ebx >>= 10) & 0x3FF) + 1;
+
+        // Total cache size is the product
+        size_t cache_total_size = cache_ways_of_associativity * cache_physical_line_partitions * cache_coherency_line_size * cache_sets;
+
+        printf(
+            "Cache ID %d:\n"
+            "- Level: %d\n"
+            "- Type: %s\n"
+            "- Sets: %d\n"
+            "- System Coherency Line Size: %d bytes\n"
+            "- Physical Line partitions: %d\n"
+            "- Ways of associativity: %d\n"
+            "- Total Size: %zu bytes (%zu kb)\n"
+            "- Is fully associative: %s\n"
+            "- Is Self Initializing: %s\n"
+            "\n"
+            , i
+            , cache_level
+            , cache_type_string
+            , cache_sets
+            , cache_coherency_line_size
+            , cache_physical_line_partitions
+            , cache_ways_of_associativity
+            , cache_total_size, cache_total_size >> 10
+            , cache_is_fully_associative ? "true" : "false"
+            , cache_is_self_initializing ? "true" : "false"
+        );
+    }
 }
