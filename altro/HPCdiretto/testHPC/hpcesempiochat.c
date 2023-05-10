33ceddd34a2275edce575e391619cdf5a8e34206
Mtvts <darkhts@hotmail.it>
2023-05-10 12:34:31 +0200

    Update hpcesempiochat.c

--- altro/HPCdiretto/testHPC/hpcesempiochat.c
+++ altro/HPCdiretto/testHPC/hpcesempiochat.c
@@ -23,14 +23,22 @@ int main(int argc, char** argv) {
     }
 
     // Processo 0 invia un messaggio a processo 1
-    if (rank == 0) {
-        data = 42;
-        MPI_Send(&data, 1, MPI_INT, 1, tag, MPI_COMM_WORLD);
+    if (rank == MPI_MAX_PROCESSOR_NAME) {
+        data = MPI_MAX_PROCESSOR_NAME;
+        MPI_Send(&data, 1, MPI_INT,  0, tag, MPI_COMM_WORLD);
         printf("Processo %d ha inviato il valore %d.\n", rank, data);
+    }else{
+          data = rank;
+        MPI_Send(&data, 1, MPI_INT,  rank + 1, tag, MPI_COMM_WORLD);
+        printf("Processo %d ha inviato il valore %d.\n", rank, data);
+  
     }
     // Processo 1 riceve il messaggio da processo 0
-    else if (rank == 1) {
-        MPI_Recv(&data, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
+    if (rank == 0) {
+        MPI_Recv(&data, 1, MPI_INT, MPI_MAX_PROCESSOR_NAME, tag, MPI_COMM_WORLD, &status);
+        printf("Processo %d ha ricevuto il valore %d.\n", rank, data);
+    }else{
+        MPI_Recv(&data, 1, MPI_INT, rank - 1, tag, MPI_COMM_WORLD, &status);
         printf("Processo %d ha ricevuto il valore %d.\n", rank, data);
     }
 
