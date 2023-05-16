00bd4a143adeb268193c50303aa9f63ab148a8eb
Mtvts <darkhts@hotmail.it>
2023-05-17 00:24:12 +0200

    varire

---
+++ altro/HPCdiretto/job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -N 4  # Numero di nodi richiesti
+#SBATCH -n 16  # Numero totale di processi MPI (processi totali = nodi x processi per nodo)
+#SBATCH -c 4   # Numero di thread OpenMP per processo
+#SBATCH -t 00:30:00  # Tempo di esecuzione previsto
+
+# Carica il modulo MPI
+module load mpi
+
+# Compila il programma
+mpicc -fopenmp mpi_openmp_example.c -o mpi_openmp_example
+
+# Esegui il programma con MPI
+srun ./mpi_openmp_example
---
+++ altro/HPCdiretto/simpleex.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <omp.h>
+
+int main(int argc, char *argv[]) {
+    int rank, size;
+    int start, end, chunk_size;
+    int i, sum = 0, partial_sum = 0;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    // Definisci il lavoro di ogni processo
+    chunk_size = 100 / size;
+    start = rank * chunk_size + 1;
+    end = start + chunk_size - 1;
+
+    // Suddivide il lavoro del for loop utilizzando OpenMP
+    #pragma omp parallel for reduction(+:partial_sum)
+    for (i = start; i <= end; i++) {
+        partial_sum += i;
+    }
+
+    // Somma parziale di ogni processo
+    printf("Processo %d - Somma parziale: %d\n", rank, partial_sum);
+
+    // Riduzione delle somme parziali di ogni processo
+    MPI_Allreduce(&partial_sum, &sum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+
+    // Stampa il risultato da ogni processo
+    printf("Processo %d - Somma totale: %d\n", rank, sum);
+
+    MPI_Finalize();
+    return 0;
+}
